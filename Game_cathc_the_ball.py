44a9fc0d94d84b74056b445b8902c649602268ce
PhysMax <prutovykh.mm@phystech.edu>
2019-10-18 15:22:50 +0300

    Добавил опользователя, подсчет очков и таблицу лиедров Для корректной работы программы необходимо добавит текстовый файл text.txt

--- Game_cathc_the_ball.py
+++ Game_cathc_the_ball.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from random import randrange as rnd, choice
 import math
-import time
 
 root = Tk()
 root.geometry('800x600')
@@ -33,40 +32,93 @@ def move_ball():
     root.after(30, move_ball)
 
 
-def new_ball(k):
+def new_ball(l):
+    """ (x,y) - center of ball, r - radius, (move_x, move_y) - vector of speed, ball - index of object """
     global x, y, r, move_x, move_y, ball
-    x.append(rnd(100, 700))
-    y.append(rnd(100, 500))
-    r.append(rnd(30, 50))
-    move_x.append(rnd(-10, 10))
-    move_y.append(rnd(-10, 10))
-    ball.append(canv.create_oval(x[k] - r[k], y[k] - r[k], x[k] + r[k], y[k] + r[k], fill=choice(colors), width=0))
+    for k in range(l):
+        x.append(rnd(100, 700))
+        y.append(rnd(100, 500))
+        r.append(rnd(30, 50))
+        move_x.append(rnd(-10, 10))
+        move_y.append(rnd(-10, 10))
+        ball.append(canv.create_oval(x[k] - r[k], y[k] - r[k], x[k] + r[k], y[k] + r[k], fill=choice(colors), width=0))
+
+
+def move_square():
+    global x_sq, y_sq, sq_move_x, sq_move_y
+    for c in range(len(square)):
+        canv.move(square[c], sq_move_x[c], sq_move_y[c])
+        x_sq[c] = x_sq[c] + sq_move_x[c]
+        y_sq[c] = y_sq[c] + sq_move_y[c]
+        if x_sq[c] + side[c] >= 800:
+            sq_move_x[c] = rnd(-25, -1)
+        elif x_sq[c] <= 0:
+            sq_move_x[c] = rnd(1, 25)
+        if y_sq[c] + side[c] >= 600:
+            sq_move_y[c] = rnd(-25, -1)
+        elif y_sq[c] <= 0:
+            sq_move_y[c] = rnd(1, 25)
+    root.after(30, move_square)
+
+
+def new_square(l):
+    """ (x_sq, y_sq) - left-upper corner of square, side - length of side square,
+     (sq_move_x, sq_move_y) - vector of speed,square - index of object """
+    global x_sq, y_sq, side, sq_move_x, sq_move_y, square
+    for k in range(l):
+        x_sq.append(rnd(100, 700))
+        y_sq.append(rnd(100, 500))
+        side.append(rnd(30, 50))
+        sq_move_x.append(rnd(-25, 25))
+        sq_move_y.append(rnd(-25, 25))
+        square.append(canv.create_rectangle(x_sq[k], y_sq[k], x_sq[k] + side[k], y_sq[k] + side[k], fill=choice(colors),
+                                            width=0))
 
 
 score = [0, 0]
 
 
 def click(event):
-    for i in range(50):
+    """ score[0] - number of points, score[1] - number of clicks """
+    score[1] += 1
+    for i in range(len(ball)):
         if math.sqrt((x[i] - event.x) ** 2 + (y[i] - event.y) ** 2) <= r[i]:
             score[0] += 1
-    score[1] += 1
-    print(score)
+    for i in range(len(square)):
+        if (event.x >= x_sq[i]) and (event.x <= x_sq[i] + side[i]) and (event.y >= y_sq[i]) and (
+                event.y <= y_sq[i] + side[i]):
+            score[0] += 2
+    score_line = Label(canv, text="Score: " + str(score[0]) + " out of " + str(score[1]), font="Arial 14")
+    score_line.place(x=0, y=30)
 
 
-global x, y, r, move_x, move_y, ball
-
-x = []
-y = []
-r = []
-move_x = []
-move_y = []
-ball = []
+user_name = input("User name: ")
 
 
-for l in range(50):
-    new_ball(l)
+def write_score():
+    global user_name, score
+    file = open('players.txt', 'a')
+    file.write(user_name + ": " + str(score) + "\n")
+    exit()
+
+
+user_line = Label(canv, text="User name: " + user_name, font="Arial 14")
+user_line.place(x=0, y=0)
+
+btn = Button(canv, text="Write score", font="Arial 14", command=write_score)
+btn.place(anchor=NE, x=800, y=0)
+
+"""sets n moving balls"""
+x, y, r, move_x, move_y, ball = [], [], [], [], [], []
+new_ball(5)
 move_ball()
 
+"""sets n moving squares"""
+x_sq, y_sq, side, sq_move_x, sq_move_y, square = [], [], [], [], [], []
+new_square(5)
+move_square()
+
+"""function for count points"""
 canv.bind('<Button-1>', click)
+
 mainloop()
